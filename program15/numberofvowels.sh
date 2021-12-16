8600994cc95403214ee19db365eed29f99804214
yashaswinishah <67494566+yashaswinishah@users.noreply.github.com>
2021-12-16 10:19:58 +0530

    Create numberofvowels.sh

---
+++ program15/numberofvowels.sh
@@ -0,0 +1,16 @@
+clear
+echo "Entre a string to find the number of Vowels "
+read st
+len=`expr $st | wc -c`
+len=`expr $len - 1`
+count=0
+while [ $len -gt 0 ]
+do
+ch=`expr $st | cut -c $len`
+case $ch in
+
+[aeiou,AEIOU]) count=`expr $count + 1` ;;
+esac
+len=`expr $len - 1`
+done
+echo "Number of vowels in the give string is $count"
